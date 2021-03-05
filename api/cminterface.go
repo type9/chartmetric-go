592dd04292aa00c1b8483d31962bca91ae0d3ad8
Gabriel Bernard Lee <gabriel@Gabriels-MacBook-Pro.local>
2021-03-05 18:07:07 +0800

    Added TODOS

--- api/cminterface.go
+++ api/cminterface.go
@@ -13,11 +13,11 @@ type CMAuth struct {
 	Key string
 }
 
-func (auth CMAuth) formatReq(req *http.Request) {
+func (auth CMAuth) formatReq(req *http.Request) { //TODO: Import refresh token using viper and gain valid refresh token
 	req.Header.Set("Authorization", "Bearer "+auth.Key)
 }
 
-//Formatter reutrn a formatted request using kwargs and authentication object
+//Formatters reutrn a formatted request using kwargs and authentication object
 
 //GetNeighborArtists has parameters (* means required ) => *id, metric (string), limit (int), type (string)
 func (auth CMAuth) GetNeighborArtists(kwargs map[string]interface{}) *http.Request {
@@ -26,6 +26,7 @@ func (auth CMAuth) GetNeighborArtists(kwargs map[string]interface{}) *http.Reque
 		log.Fatalln(err)
 	}
 
+	//TODO: Fix URL formatting such that optional parameters are ommitted if null
 	url := fmt.Sprintf("https://api.chartmetric.com/api/artist/%d/neighboring-artists?metric=%s&limit=%d", kwargs["id"], kwargs["metric"], kwargs["limit"])
 
 	req, err := http.NewRequest("GET", url, bytes.NewBuffer(reqBody))
